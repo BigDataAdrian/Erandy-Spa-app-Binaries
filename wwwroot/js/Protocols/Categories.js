4726b7df5cf61d550970bf333ee2d8ed395e708a
uid39544 <uid39544@GLLF025W>
2026-07-18 16:54:07 -0600

    wip

---
+++ wwwroot/js/Protocols/Categories.js
@@ -0,0 +1,254 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    setActiveMenu("CategoriesModule", "CategoriesModuleHome");
+    LoadCategories();
+
+    const BtnDeleteCategory = document.getElementById('BtnDeleteCategory');
+    BtnDeleteCategory.addEventListener('click', async () => {
+        await DeleteCategory();
+    });
+
+    const BtnCreateCategoryModal = document.getElementById('BtnCreateCategoryModal');
+    BtnCreateCategoryModal.addEventListener('click', async () => {
+        await CreateCategoryModalOpen();
+    });
+
+    const BtnCreateQuestion = document.getElementById('BtnCreateCategory');
+    BtnCreateQuestion.addEventListener('click', async () => {
+        await AddCategory();
+    });
+
+    const BtnUpdateCategory = document.getElementById('BtnUpdateCategory');
+    BtnUpdateCategory.addEventListener('click', async () => {
+        await UpdateCategory();
+    });
+});
+
+async function LoadCategories() {
+    try {
+        const response = await fetch(`/Protocols/GetCategories`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (response.status >= 200 && response.status <= 299) {
+            const result = await response.json().catch(() => null);
+
+            const tbody = document.querySelector("#CategoriesTable tbody");
+
+            tbody.innerHTML = "";
+            result.forEach(c => {
+                const tr = document.createElement("tr");
+
+                let Check = "";
+                if (c.enabled == true) {
+                    Check = "checked";
+                }
+
+                tr.setAttribute("data-id", c.id);
+                tr.innerHTML = `
+                    <td>${c.name}</td>
+                    <td>${c.description}</td>
+                    <td>
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="CategoryMode?${c.id}" ${Check} disabled>
+                            <label class="form-check-label" for="CategoryMode?${c.id}"></label>
+                        </div>
+                    </td>
+                    <td> 
+                        <div class="btn-group float-end" role="group" aria-label="Acciones">
+                            <button  onclick="DeleteCategoryModalOpen(${c.id},'${c.name}')" class="btn btn-outline-danger">
+                                <i class="bi bi-trash"></i>
+                            </button>
+                            <button onclick="UpdateCategoryModalOpen(${c.id},'${c.name}','${c.description}',${c.enabled})" class="btn btn-outline-warning">
+                                <i class="bi bi-pencil"></i>
+                            </button>
+                        </div>
+                    </td>
+            `;
+
+                tbody.appendChild(tr);
+
+            });
+        }
+
+
+        if (response.status >= 400 && response.status <= 499) {
+            const result = await response.text().catch(() => null);
+            showToast("warning", result);
+        }
+
+        if (response.status >= 500 && response.status <= 599) {
+            const result = await response.text().catch(() => null);
+            showToast("danger", result);
+        }
+
+    } catch (error) {
+        showToast("danger", error);
+    }
+}
+function DeleteCategoryModalOpen(Id, Question) {
+    document.getElementById("LabelModalDeleteCategory").innerText = Question;
+    sessionStorage.setItem('CategoryIdSelected', Id);
+    var DeleteCategoryModal = new bootstrap.Modal(document.getElementById("DeleteCategoryModal"));
+    DeleteCategoryModal.show();
+}
+async function DeleteCategory() {
+    try {
+        var Id = sessionStorage.getItem('CategoryIdSelected');
+        const data = {
+            CategoryId: Id
+        };
+        const response = await fetch("/Protocols/DeleteCategory", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.status >= 200 && response.status <= 299) {
+            const result = await response.text().catch(() => null);
+            showToast("success", result);
+            const DeleteCategoryModal = document.getElementById('DeleteCategoryModal');
+            const modalInstance = bootstrap.Modal.getInstance(DeleteCategoryModal);
+
+            if (!modalInstance) {
+                new bootstrap.Modal(modalElement).hide();
+            } else {
+                modalInstance.hide();
+            }
+
+            LoadCategories();
+        }
+
+        if (response.status == 400 && response.status <= 499) {
+            const result = await response.text().catch(() => null);
+            showToast("warning", result);
+        }
+
+        if (response.status == 500 && response.status <= 599) {
+            const result = await response.text().catch(() => null);
+
+            showToast("danger", result);
+        }
+    } catch (error) {
+        showToast("danger", error);
+    }
+}
+function CreateCategoryModalOpen() {
+    var CreateCategoryModal = new bootstrap.Modal(document.getElementById("CreateCategoryModal"));
+    CreateCategoryModal.show();
+}
+async function AddCategory() {
+    try {
+        const Name = document.getElementById("CreateModalName");
+        const Description = document.getElementById("CreateModalDescription");
+        const Enabled = document.getElementById("CreateModalEnabled");
+        const data = {
+            Name: Name.value,
+            Description: Description.value,
+            Enabled: Enabled.checked
+        };
+        const response = await fetch("/Protocols/AddCategory", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.status >= 200 && response.status <= 299) {
+            const result = await response.text().catch(() => null);
+
+            document.getElementById("CreateModalName").value = "";
+            document.getElementById("CreateModalDescription").value = "";
+            document.getElementById("CreateModalEnabled").checked = false;
+
+            showToast("success", result);
+            const CreateCategoryModal = document.getElementById('CreateCategoryModal');
+            const modalInstance = bootstrap.Modal.getInstance(CreateCategoryModal);
+
+            if (!modalInstance) {
+                new bootstrap.Modal(modalElement).hide();
+            } else {
+                modalInstance.hide();
+            }
+            LoadCategories();
+
+        }
+
+        if (response.status == 400 && response.status <= 499) {
+            const result = await response.text().catch(() => null);
+            showToast("warning", result);
+        }
+
+        if (response.status == 500 && response.status <= 599) {
+            const result = await response.text().catch(() => null);
+
+            showToast("danger", result);
+        }
+    } catch (error) {
+        showToast("danger", error);
+    }
+}
+async function UpdateCategory() {
+    try {
+        const Id = sessionStorage.getItem('CategoryIdSelected');
+        const Name = document.getElementById("UpdateModalName");
+        const Description = document.getElementById("UpdateModalDescription");
+        const Enabled = document.getElementById("UpdateEnabled");
+        const data = {
+            Id: Id,
+            Name: Name.value,
+            Description: Description.value,
+            Enabled: Enabled.checked
+        };
+        const response = await fetch("/Protocols/UpdateCategory", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.status >= 200 && response.status <= 299) {
+            const result = await response.text().catch(() => null);
+            showToast("success", result);
+            const UpdateCategoryModal = document.getElementById('UpdateCategoryModal');
+            const modalInstance = bootstrap.Modal.getInstance(UpdateCategoryModal);
+
+            if (!modalInstance) {
+                new bootstrap.Modal(modalElement).hide();
+            } else {
+                modalInstance.hide();
+            }
+            LoadCategories();
+
+        }
+
+        if (response.status == 400 && response.status <= 499) {
+            const result = await response.text().catch(() => null);
+            showToast("warning", result);
+        }
+
+        if (response.status == 500 && response.status <= 599) {
+            const result = await response.text().catch(() => null);
+
+            showToast("danger", result);
+        }
+    } catch (error) {
+        showToast("danger", error);
+    }
+}
+function UpdateCategoryModalOpen(Id, Name, Description, Enabled) {
+
+    sessionStorage.setItem('CategoryIdSelected', Id);
+    document.getElementById("UpdateModalName").value = Name;
+    document.getElementById("UpdateEnabled").checked = Enabled;
+    document.getElementById("UpdateModalDescription").value = Description;
+
+    var UpdateCategoryModal = new bootstrap.Modal(document.getElementById("UpdateCategoryModal"));
+    UpdateCategoryModal.show();
+}
---
+++ wwwroot/js/Protocols/Index.js
@@ -0,0 +1,165 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    LoadCategories();
+
+    document.getElementById("CategoriesSelect").addEventListener("change", function () {
+        LoadProtocols();
+    });
+});
+
+async function LoadCategories() {
+    try {
+        const response = await fetch(`/Protocols/GetCategoriesSelect`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (response.status >= 200 && response.status <= 299) {
+            const result = await response.json().catch(() => null);
+
+            const select = document.getElementById("CategoriesSelect");
+
+            select.innerHTML = '<option value="" selected disabled>Seleccione una categoria...</option>';
+
+            if (result && result.length > 0) {
+                result.forEach(item => {
+                    const option = document.createElement("option");
+                    option.value = item.id;
+                    option.textContent = item.name;
+                    select.appendChild(option);
+                });
+            }
+        }
+
+
+        if (response.status >= 400 && response.status <= 499) {
+            const result = await response.text().catch(() => null);
+            showToast("warning", result);
+        }
+
+        if (response.status >= 500 && response.status <= 599) {
+            const result = await response.text().catch(() => null);
+            showToast("danger", result);
+        }
+
+    } catch (error) {
+        showToast("danger", error);
+    }
+}
+async function LoadProtocols() {
+    const select = document.getElementById("CategoriesSelect");
+    document.getElementById('CardData').style.display = "flex";
+    try {
+        const response = await fetch(`/Protocols/GetProtocols?CategoryId=${select.value}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (response.status >= 200 && response.status <= 299) {
+            const result = await response.json().catch(() => null);
+
+            const tableBody = document.querySelector("#ProtocolsTable tbody");
+            tableBody.innerHTML = "";
+
+            if (result && result.length > 0) {
+                result.forEach(item => {
+                    const row = `
+                    <tr>
+                        <td>${item.name}</td>
+                        <td>${item.description}</td>
+                        <td class="text-end">
+                            <button class="btn btn-sm btn-outline-secondary" 
+                                onclick="EditHours(${item.id})">
+                                <i class="bi bi-pencil"></i>
+                            </button>
+                        </td>
+                    </tr>`;
+                    tableBody.innerHTML += row;
+                });
+            }
+        }
+
+        if (response.status >= 400 && response.status <= 499) {
+            const result = await response.text().catch(() => null);
+            showToast("warning", result);
+        }
+
+        if (response.status >= 500 && response.status <= 599) {
+            const result = await response.text().catch(() => null);
+            showToast("danger", result);
+        }
+
+    } catch (error) {
+        showToast("danger", error);
+    }
+}
+function EditHours(id, day, start, end) {
+    const daySpan = document.getElementById('SelectedDayName');
+    const inputStart = document.getElementById('UpdateStartTime');
+    const inputEnd = document.getElementById('UpdateEndTime');
+    const btnUpdate = document.getElementById('btnUpdateZone');
+
+    daySpan.textContent = day;
+
+    inputStart.value = (start === "No establecido") ? "" : start;
+    inputEnd.value = (end === "No establecido") ? "" : end;
+
+    btnUpdate.setAttribute('data-id', id);
+
+    const modalInstance = new bootstrap.Modal(document.getElementById('UpdateHourModal'));
+    modalInstance.show();
+}
+async function UpdateWorkingTime() {
+    try {
+        const id = document.getElementById("btnUpdateZone").getAttribute("data-id");
+        const start = document.getElementById("UpdateStartTime").value;
+        const end = document.getElementById("UpdateEndTime").value;
+
+        if (!start || !end) {
+            showToast("warning", "Por favor, complete ambos horarios.");
+            return;
+        }
+
+        const data = {
+            Id: parseInt(id),
+            Start: start,
+            End: end
+        };
+
+        const response = await fetch(`/Therapists/UpdateWorkingTime`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.status >= 200 && response.status <= 299) {
+            const result = await response.text().catch(() => null);
+            showToast("success", result);
+
+            const modalElement = document.getElementById('UpdateHourModal');
+            const modalInstance = bootstrap.Modal.getInstance(modalElement);
+            modalInstance.hide();
+
+            const therapistsSelect = document.getElementById("therapistsSelect").value;
+            LoadWorkingHours(therapistsSelect);
+        }
+
+        if (response.status >= 400 && response.status <= 499) {
+            const result = await response.text().catch(() => null);
+            showToast("warning", result);
+        }
+
+        if (response.status >= 500 && response.status <= 599) {
+            const result = await response.text().catch(() => null);
+            showToast("danger", result);
+        }
+
+    } catch (error) {
+        showToast("danger", error);
+    }
+} 
